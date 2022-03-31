f38d87dc446d2a70dc68b6300433a3d2935e1035
Paul Dawoon Kim <91786930+ITDawoon@users.noreply.github.com>
2022-03-31 13:48:27 +0000

    - Made fake database that store the "jobs"

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
 
 app = Flask("SuperScrapper")
+
+db = {}
 
 @app.route("/")
 def home():
   return render_template("potato.html")
+
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  if word:
+    word = word.lower()
+    fromDb = db.get(word)
+    if fromDb:
+      jobs = fromDb
+    else:
+      jobs = get_jobs(word)
+      db[word] = jobs
+  else:
+    return redirect("/")
+  return render_template("report.html", 
+  searchingBy=word,
+  resultsNumber=len(jobs)
+  )
 
 app.run(host="0.0.0.0")
